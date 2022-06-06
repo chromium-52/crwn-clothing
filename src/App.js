84c86e64a0ed1342f02c84566b8ed17d8334e938
Kenny Jung <dukyoung01@naver.com>
2022-06-06 19:26:23 -0400

    Create sign in form and authentication page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -21,7 +21,7 @@ const App = () => {
         {/* index - if passed, render the component if the url matches that of its parent */}
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
